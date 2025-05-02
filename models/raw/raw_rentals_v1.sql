06f45c62aad380e6020aa444636f26a36c180eb6
AntoineGiraud <antoine.giraud@outlook.com>
2025-05-02 15:19:49 +0200

    fix(raw): adjust loading of rentals & station v1
    - 2021 rentals had a different header from 2014->2020
    --> 2021 in a separate folder

--- models/raw/raw_rentals_v1.sql
+++ models/raw/raw_rentals_v1.sql
@@ -1,11 +1,26 @@
-
-
+-- les fichiers de 2014 à 2020 respectent la mm norme :)
 SELECT
-    * replace( -- 👇 truncate datetime to minutes & cast
-        concat(left(start_date::string, 16),':00')::datetime as start_date,
-        concat(left(end_date::string, 16),':00')::datetime as end_date
-    )
+    concat(left(start_date::string, 16),':00')::datetime as start_date, -- déjà tronqué à la minute
+    start_station_code::int as start_station_code,
+    concat(left(end_date::string, 16),':00')::datetime as end_date, -- mais on en sait jamais
+    end_station_code::int as end_station_code,
+    duration_sec::int as duration_sec,
+    is_member::int as is_member,
+    filename,
 from {{ source('rentals', 'rentals_v1') }}
-    {# --read_csv( 'C:\Users\antoi\Documents\data\bixi\data_od_bixi_csv_v1\'
-    --   || '**\OD_20*.csv',
-    --   header=true, AUTO_DETECT=TRUE, ignore_errors=true) #}
+ {# --read_csv('~\Documents\codes\dbt_bixi_opendata\input/rentals_v1/*.csv', filename=true, all_varchar=1) #}
+where TRY_CAST(start_station_code as integer) is not null -- station test
+
+union all
+
+-- le fichier de 2021 n'ayant pas respecté le standard 2020 et avant
+SELECT
+    concat(left(start_date::string, 16),':00')::datetime as start_date, -- grain au miliseconde ...
+    emplacement_pk_start::int as start_station_code,
+    concat(left(end_date::string, 16),':00')::datetime as end_date,
+    emplacement_pk_end::int as end_station_code,
+    duration_sec::int as duration_sec,
+    is_member::int as is_member,
+    filename,
+from {{ source('rentals', 'rentals_v1_2021') }}
+where TRY_CAST(emplacement_pk_start as integer) is not null
